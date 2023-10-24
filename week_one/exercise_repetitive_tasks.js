a75faf1ece3422d2cb1a0b3d55701409d128a923
Mahmoud Alwakeel <alwakeel@Mahmouds-MacBook-Pro.local>
2023-10-24 20:55:34 +0300

    week one done

---
+++ week_one/exercise_repetitive_tasks.js
@@ -0,0 +1,85 @@
+/* Task 1
+
+Write a "for" loop that will perform exactly the same repetitive code as this:
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+*/
+console.log("----- task1 ------")
+
+for(var i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+/* Task 2
+
+Write a "for" loop that will perform exactly the same repetitive code as this:
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+*/
+console.log("----- task2 ------")
+
+for(var i = 5; i >= 1; i--) {
+    console.log(i)
+}
+
+/* Task 3
+Write a "while" loop that will perform exactly the same repetitive code as this:
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+*/
+console.log("----- task3 ------")
+
+var i = 1
+while(i <=5) {
+    console.log(i)
+    i += 1
+}
+
+/* Task 4
+
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+*/
+console.log("----- task4 ------")
+var j = 5
+while(j >= 1) {
+    console.log(j)
+    j -= 1
+}
+console.log("countdown finished")
+
+
+/* Task 5
+
+Write a "while" loop that will perform exactly the same repetitive code as this:
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+*/
+console.log("------ task5 -----")
+var year = 2018
+while(year <= 2022) {
+    console.log(year)
+    year+= 1
+}
---
+++ week_one/firstFile.js
@@ -0,0 +1,10 @@
+/*
+this is not my first javascript code, 
+i was learning javascript since 2 years ago but i didn'y continue
+and here we are back again with bigger knwoledge of programming world
+learning javascript again
+*/
+console.log('weka is coming')
+
+var petDog = 'Rex'; // Task 1 solution
+console.log(petDog);
